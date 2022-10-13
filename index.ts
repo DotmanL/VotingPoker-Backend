b722ba32082e416db8e5625732a66c230cfbe7b1
Oladotun <oladotun@bindystreet.com>
2022-10-13 19:36:32 +0100

    socket setup

---
+++ index.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+const app = express();
+const http = require('http').Server(app);
+const cors = require('cors');
+const PORT = process.env.PORT || 4000;
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+
+
+socketIO.on('connection', (socket) => {
+  console.log(`${socket.id} user connected!`);
+
+  socket.on('votes', (data) => {
+    console.log(data, "votes response");
+    socketIO.emit('votesResponse', data);
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log(' A user disconnected');
+  });
+});
+
+app.get('/api', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
