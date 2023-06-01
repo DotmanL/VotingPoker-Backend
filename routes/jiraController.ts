3ad4969f57a48828801edc2fd7da4f5a01a96a79
LAPTOP-KK2TV0BP\doran <oladotunlawal7@gmail.com>
2023-06-01 22:40:09 +0100

    fix: endpoint

--- routes/jiraController.ts
+++ routes/jiraController.ts
@@ -310,7 +310,7 @@ router.get("/getMyFilters/:userId", async (req, res) => {
     );
 
     const response = await axios.get(
-      `https://api.atlassian.com/ex/jira/${siteDetails.data[0].id}/rest/api/3/filter/my`,
+      `https://api.atlassian.com/ex/jira/${siteDetails.data[0].id}/rest/api/3/filter/search`,
       {
         headers: {
           Accept: "application/json",
